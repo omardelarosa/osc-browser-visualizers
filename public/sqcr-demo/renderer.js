6c4140ca427c5cbf7ad7c2e47d66c2d7cc2f6ed8
Omar Delarosa <thedelarosa@gmail.com>
2018-07-11 19:09:31 -0400

    added grid to sqcr demo

--- public/sqcr-demo/renderer.js
+++ public/sqcr-demo/renderer.js
@@ -1,3 +1,25 @@
+const makeRow = (cols, idx = 1) => {
+    const colDivs = [];
+
+    for (let i = 0; i < cols; i++) {
+        colDivs.push(
+            `<div class="k-col k-c-16 k-center blank-box k-${i + 1}"></div>`,
+        );
+    }
+
+    return `<div class="k-row r-${idx}">${colDivs.join('\n')}</div>`;
+};
+
+const makeGrid = (rows, cols) => {
+    const rowsHTML = [];
+
+    for (let i = 0; i < rows; i++) {
+        rowsHTML.push(makeRow(cols, i + 1));
+    }
+
+    return `<div class="container">${rowsHTML.join('\n')}</div>`;
+};
+
 module.exports = (locals, scripts, scriptTags) => `
     <!DOCTYPE html>
     <html>
@@ -6,18 +28,18 @@ module.exports = (locals, scripts, scriptTags) => `
             <meta charset="UTF-8" />
             <link rel="stylesheet" type="text/css" media="all" href="/public/css/grid.css"></link>
             <link rel="stylesheet" type="text/css" media="all" href="/public/css/global.css"></link>
+
+            <link rel="stylesheet" type="text/css" media="all" href="/public/matrix-16x8/styles.css"></link>
             <link rel="stylesheet" type="text/css" media="all" href="/public/sqcr-demo/styles.css"></link>
             ${scriptTags.join('\n')}
         </head>
 
         <body>
-        <pre>
-            ${locals.ASCII_TEXT}
-        </pre>
-        <div>
-            <a href="javascript:sqcr.start()">start</a>
-            <a href="javascript:sqcr.stop()">stop</a>
-        </div>
+            ${makeGrid(8, 16)}
+            <div>
+                <a href="javascript:sqcr.start()">start</a>
+                <a href="javascript:sqcr.stop()">stop</a>
+            </div>
  
         </body>
     </html>
