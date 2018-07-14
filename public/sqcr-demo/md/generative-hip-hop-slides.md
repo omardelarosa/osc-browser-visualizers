ee4466ba5c416116c19ad8192e8d81550feb228b
Omar Delarosa <thedelarosa@gmail.com>
2018-07-14 13:26:34 -0400

    added basic boilerplate for pres

---
+++ public/sqcr-demo/md/generative-hip-hop-slides.md
@@ -0,0 +1,40 @@
+class: center, middle
+
+# Making Self-Generating Hip Hop in JS
+
+---
+
+# Markov Chains
+
+TBD
+
+---
+
+# Music Theory Basics
+
+--
+
+-   ## note
+
+--
+
+-   ## key
+
+--
+
+-   ## chord
+
+--
+
+-   ## key
+
+---
+
+# Conclusion In Progress
+
+```typescript
+interface Foo {
+    hello: string;
+    world: number;
+}
+```
---
+++ public/sqcr-demo/renderers/slider.js
@@ -0,0 +1,31 @@
+const fs = require('fs');
+const path = require('path');
+
+const getMarkdown = pathToMarkdown => {
+    const absPath = path.join(process.cwd(), pathToMarkdown);
+    try {
+        return fs.readFileSync(absPath).toString();
+    } catch (e) {
+        console.error('Markdown read error: ', e.message);
+        return '';
+    }
+};
+
+module.exports = (locals, scripts, scriptTags) => `
+    <!DOCTYPE html>
+    <html>
+      <head>
+        <link rel="stylesheet" type="text/css" media="all" href="/public/sqcr-demo/css/slides.css"></link>
+      </head>
+      <body>
+        <!-- TODO: self host locally? -->
+        <script src="https://remarkjs.com/downloads/remark-latest.min.js"></script>
+        <script>
+var slideshow = remark.create({
+    sourceUrl: "${locals.MARKDOWN_PATH}",
+    highlightStyle: "monokai"
+});
+        </script>
+      </body>
+    </html>
+`;
